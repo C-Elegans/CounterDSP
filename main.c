633732059e0f32dc2ad0bd57b94b732ae334c744
C-Elegans <mtnolan2640@gmail.com>
2018-01-11 12:02:56 -0500

    Move configuration to config.h

--- main.c
+++ main.c
@@ -4,55 +4,9 @@
  *
  * Created on December 27, 2017, 4:14 PM
  */
-// DSPIC33FJ128GP802 Configuration Bit Settings
-
-// 'C' source line config statements
-
-// FBS
-#pragma config BWRP = WRPROTECT_OFF     // Boot Segment Write Protect (Boot Segment may be written)
-#pragma config BSS = NO_FLASH           // Boot Segment Program Flash Code Protection (No Boot program Flash segment)
-#pragma config RBS = NO_RAM             // Boot Segment RAM Protection (No Boot RAM)
-
-// FSS
-#pragma config SWRP = WRPROTECT_OFF     // Secure Segment Program Write Protect (Secure segment may be written)
-#pragma config SSS = NO_FLASH           // Secure Segment Program Flash Code Protection (No Secure Segment)
-#pragma config RSS = NO_RAM             // Secure Segment Data RAM Protection (No Secure RAM)
-
-// FGS
-#pragma config GWRP = OFF               // General Code Segment Write Protect (User program memory is not write-protected)
-#pragma config GSS = OFF                // General Segment Code Protection (User program memory is not code-protected)
-
-// FOSCSEL
-#pragma config FNOSC = FRC              // Oscillator Mode (Internal Fast RC (FRC))
-#pragma config IESO = OFF               // Internal External Switch Over Mode (Start-up device with user-selected oscillator source)
-
-// FOSC
-#pragma config POSCMD = NONE            // Primary Oscillator Source (Primary Oscillator Disabled)
-#pragma config OSCIOFNC = ON            // OSC2 Pin Function (OSC2 pin has digital I/O function)
-#pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow Multiple Re-configurations)
-#pragma config FCKSM = CSDCMD           // Clock Switching and Monitor (Both Clock Switching and Fail-Safe Clock Monitor are disabled)
-
-// FWDT
-#pragma config WDTPOST = PS32768        // Watchdog Timer Postscaler (1:32,768)
-#pragma config WDTPRE = PR128           // WDT Prescaler (1:128)
-#pragma config WINDIS = OFF             // Watchdog Timer Window (Watchdog Timer in Non-Window mode)
-#pragma config FWDTEN = OFF             // Watchdog Timer Enable (Watchdog timer enabled/disabled by user software)
-
-// FPOR
-#pragma config FPWRT = PWR128           // POR Timer Value (128ms)
-#pragma config ALTI2C = OFF             // Alternate I2C  pins (I2C mapped to SDA1/SCL1 pins)
-
-// FICD
-#pragma config ICS = PGD1               // Comm Channel Select (Communicate on PGC1/EMUC1 and PGD1/EMUD1)
-#pragma config JTAGEN = OFF             // JTAG Port Enable (JTAG is Disabled)
-
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
-#include <xc.h>
 
 
-
+#include "config.h"
 #include <dsp.h>
 #include <uart.h>
 #include <adc.h>
@@ -63,17 +17,6 @@
 
 fractional dmabuf1[512] __attribute__((space(dma)));
 fractional dmabuf2[512] __attribute__((space(dma)));
-/*
-_FOSCSEL(FNOSC_FRCPLL);
-_FOSC(OSCIOFNC_OFF & POSCMD_NONE);
-_FWDT(FWDTEN_OFF);
-_FICD(JTAGEN_OFF & ICS_PGD1);
-void setup_pll(void){
-    CLKDIVbits.PLLPRE=0;        // PLLPRE (N2) 0=/2
-    PLLFBD=41;                  // pll multiplier (M) = +2
-    CLKDIVbits.PLLPOST=0;       // PLLPOST (N1) 0=/2
-       while(!OSCCONbits.LOCK);    // wait for PLL ready
-}*/
 void configure_adc(void){
 
 
