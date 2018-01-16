1aab91db4cef83e112cb4593b73ac5c05ac94311
C-Elegans <mtnolan2640@gmail.com>
2018-01-16 13:16:10 -0500

    Use FFT for convolution instead of plain convolution

--- matlab/analysis.m
+++ matlab/analysis.m
@@ -1,21 +1,24 @@
-samples = csvread('../data/samples3.txt',1,0); % skip header
+samples = csvread('../data/samples6.txt',1,0); % skip header
 samplerate = 5000;
 sampletime = 1/samplerate;
 samples = samples-samples(1);
+samples = samples.';
 times = (1:length(samples)) * sampletime;
 figure(1);
 plot(times,samples);
 
-period = 0.015;
-ctimes = period:-sampletime:0;
+period = 0.012;
+ctimes = 0:sampletime:period;
 corr = sin(ctimes*(1/period)*2*pi);
+corr = horzcat(corr, zeros(1,1024-length(corr)));
 scale = sum(corr.^2);
 figure(2);
 
-fr = (0:length(times)-1)*samplerate/length(times);
-plot(fr, abs(fft(samples)));
+fr = (0:length(times)*2+1)*samplerate/length(times);
+S = fft(horzcat(samples, zeros(1,513)));
+plot(fr, abs(S));
 xlim([0,300]);
 figure(3);
 
-convolved = conv(samples,corr)/scale;
+convolved = ifft(fft(corr) .* S)/scale;
 plot(convolved);
