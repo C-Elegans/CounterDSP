3ca948fb025c7f08506c318e555ff9d91a6f025a
C-Elegans <mtnolan2640@gmail.com>
2018-01-20 12:36:08 -0500

    Add some routines for signal processing
    scalepadbuffer - Scales input signal to -0.5-0.5, centers it around 0,
    and copies it to the destination buffer with 0 padding
    
    vmul - multiplies a transformed signal by another

--- src/convolve.s
+++ src/convolve.s
@@ -29,5 +29,52 @@ CDONE:	SAC A, #0, [W0++]
     pop W8
 return
     
-    
+_scalepadbuffer:
+	;; W0 = pointer to the collected samples
+	;; W1 = pointer to the destination buffer
+
+	;; Normalize the samples to 0 and divide by 2 for fft
+	MOV [W0], W3
+	DO #511, DONE1
+	MOV [W0++], W2
+	SUB W2, W3, W2
+	ASR W2, #1, W2
+DONE1:	MOV W2, [W1++]
+
+	;; Pad the buffer with 0s
+	MOV #0, W2
+	ADD W1, #2, W0
+	REPEAT #511
+	MOV W2, [W1++]
+	return
+.global _vmul
+_vmul:
+	
+	;; W0 = source buffer 1 / dest buffer
+	;; W1 = source buffer 2
+	;; W2 = Count
+	push W13
+	push W10
+	push W8
+	MOV W0, W13
+	MOV W0, W8
+	MOV W1, W10
+	LSR W2, #1, W2
+	SUB W2, #1, W2
+
+	;; Prime the loop
+	CLR A, [W8]+=2, W4, [W10]+=2, W5
+	MPY W4*W5, B 
+	DO W2, VMULD1
+	MOVSAC A, [W8]+=2, W4, [W10]+=2, W5, [W13]+=2
+	MPY W4*W5, A
+	MOVSAC B, [W8]+=2, W4, [W10]+=2, W5, [W13]+=2
+VMULD1:	MPY W4*W5, B
+
+
+	pop W8
+	pop W10
+	pop W13
+	
+	
     
