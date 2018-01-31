6fa4dee5e7c3ce556e124cda9d577aa4381c0d4a
C-Elegans <mtnolan2640@gmail.com>
2018-01-31 14:15:57 -0500

    Change the targets of DO loops to use local labels

--- src/convolve.s
+++ src/convolve.s
@@ -3,43 +3,43 @@
     
 .global _convolve
 _convolve: 
-    push W8
-    push W10
-    ;W0 is the pointer to the data in X space (signal)
-    ;W1 is the length
-    ;W2 is the pointer to the data in Y space (filter/IR)
-    ;W3 is the ydata length
+	push W8
+	push W10
+	;;W0 is the pointer to the data in X space (signal)
+	;;W1 is the length
+	;;W2 is the pointer to the data in Y space (filter/IR)
+	;;W3 is the ydata length
+	
+	
+	;;W6 and W7 will be the data operands for the MAC instructions
+	sub #1, W1
+	sub #2, W3
+	
     
-    
-    ;W6 and W7 will be the data operands for the MAC instructions
-    sub #1, W1
-    sub #2, W3
-    
-    
-    DO W1, CDONE
+	DO W1, 1f
 	mov W0, W8 ;W8 will be the MAC pointer for data1
 	mov W2, W10 ;W10 will be the MAC pointer for data2	
 	clr A, [W8]+=2, W6, [W10]+=2, W7
 	REPEAT W3
-	    MAC  W6*W7,A, [W8]+=2, W6, [W10]+=2, W7
+	MAC  W6*W7,A, [W8]+=2, W6, [W10]+=2, W7
 	MAC  W6*W7,A
-CDONE:	SAC A, #0, [W0++]
-    
-    pop W10
-    pop W8
+1:	SAC A, #0, [W0++]
+	
+	pop W10
+	pop W8
 return
     
 _scalepadbuffer:
 	;; W0 = pointer to the collected samples
 	;; W1 = pointer to the destination buffer
-
+	
 	;; Normalize the samples to 0 and divide by 2 for fft
 	MOV [W0], W3
-	DO #511, DONE1
+	DO #511, 1f
 	MOV [W0++], W2
 	SUB W2, W3, W2
 	ASR W2, #1, W2
-DONE1:	MOV W2, [W1++]
+1:	MOV W2, [W1++]
 
 	;; Pad the buffer with 0s
 	MOV #0, W2
@@ -64,9 +64,9 @@ _vmul:
 	;; Prime the loop
 	CLR A, [W8]+=2, W4, [W10]+=2, W5
 	MPY W4*W5, A 
-	DO W2, VMULD1
+	DO W2, 1f
 	MOVSAC B, [W8]+=2, W4, [W10]+=2, W5, [W13]+=2
-VMULD1: MPY W4*W5, A 
+1: MPY W4*W5, A 
 
 
 	pop W8
@@ -102,17 +102,14 @@ _count_spikes:			; W0 - pointer to data, W1 - count,
 	mov #0, W5
 	sub W1, #1, W1
 
-	DO W1, countend
+	DO W1, 1f
 	mov [W3++], W4
 	cp W4, W2
 	rlc W5, W5		; shift C into W5
 	cp W5, #0b1111		; If Spike lasted 4 samples
 	bra NZ, 1f
 	add W0, #1, W0
-
-1:	
-countend:
-	nop
+1: 	nop
 
 	return
 	
