52d9ad515c7d99cbb19ce2de9db8d55b7099495d
C-Elegans <mtnolan2640@gmail.com>
2018-02-14 10:54:04 -0500

    Modify matlab script to generate nice(r) figures

--- matlab/analysis.m
+++ matlab/analysis.m
@@ -5,12 +5,16 @@ samples = samples-samples(1);
 
 samples = samples.';
 times = (1:length(samples)) * sampletime;
-samples = samples + pinknoise(length(samples)).*0.00;
+samples = samples + pinknoise(length(samples)).*0.02;
 figure(1);
-plot(times,samples);
+plot(times,samples, 'LineWidth', 1);
 set(gca, 'FontSize', 14);
+title('Noisy input signal');
+
 xlabel("Seconds", 'FontSize', 16);
 ylabel("Volts", 'FontSize', 16);
+xlim([0,0.1]);
+print('plot1', '-dpng');
 period = 0.012;
 frequency = 1/period;
 ctimes = 0:sampletime:period;
@@ -24,14 +28,44 @@ figure(2);
 fr = (0:length(times)*2-1)*samplerate/length(times);
 S = fft(horzcat(samples, zeros(1,512)));
 %plot(fr, abs(S));
-plot(corr);
-xlim([0,300]);
+plot(ctimes, corr, 'LineWidth', 1);
+
+set(gca, 'FontSize', 14);
+title('Expected signal');
+
+xlabel("Seconds", 'FontSize', 16);
+ylabel("Volts", 'FontSize', 16);
+
+print('plot2', '-dpng');
 figure(3);
 
 %convolved = ifft(fft(corr) .* S)/scale;
 %plot(convolved);
 out = xcorr(samples,corr/scale);
-plot(out.^2);
+otimes = (1:length(out)) * sampletime;
+plot(otimes, out, 'LineWidth', 1);
+set(gca, 'FontSize', 14);
+title('Convolved signal');
+
+xlabel("Seconds", 'FontSize', 16);
+ylabel("Units", 'FontSize', 16);
+
+print('plot3', '-dpng');
+
+figure(4);
+hold on
+plot(otimes, out.^2, 'LineWidth', 1);
+plot(otimes, repmat(0.09^2,length(out)),'--', 'LineWidth', 1);
+hold off
+xlim([0,otimes(length(otimes))]);
+legend('signal', 'threshold');
+set(gca, 'FontSize', 14);
+title('Convolved signal');
+
+xlabel("Seconds", 'FontSize', 16);
+ylabel("Units", 'FontSize', 16);
+
+print('plot4', '-dpng');
 
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
